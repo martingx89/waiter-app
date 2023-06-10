b8107ab8e9309c43a94b4b5ea8bf95ad40c734e9
Marcin Adamiak <marcin_89@outlook.com>
2023-06-10 17:32:27 +0200

    Prepare actions

--- src/redux/tableRedux.js
+++ src/redux/tableRedux.js
@@ -4,9 +4,11 @@ export const getAllTables = (state) => state.tables;
 // actions
 const createActionName = (actionName) => `app/tables/${actionName}`;
 export const SHOW_TABLES = createActionName('SHOW_TABLES');
+export const UPDATE_TABLE = createActionName('UPDATE_TABLE');
 
 // action creators
 export const showTables = (payload) => ({ type: SHOW_TABLES, payload });
+export const updateTable = (payload) => ({ type: UPDATE_TABLE, payload });
 
 export const fetchTables = () => {
   return (dispatch) => {
@@ -20,6 +22,8 @@ const tablesReducer = (statePart = [], action) => {
   switch (action.type) {
     case SHOW_TABLES:
       return [...action.payload];
+    case UPDATE_TABLE:
+      return statePart.map((table) => (table.id === action.payload.id ? { ...table, ...action.payload } : table));
     default:
       return statePart;
   }
