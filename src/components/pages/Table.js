7785195877f9406d80b1e6bc4d48dc244d8bdfc8
Marcin Adamiak <marcin_89@outlook.com>
2023-06-10 18:55:56 +0200

    Add update functions

--- src/components/pages/Table.js
+++ src/components/pages/Table.js
@@ -1,21 +1,61 @@
-import { Form, Row, Col, Stack, Button } from 'react-bootstrap';
-import { useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
 import { useParams } from 'react-router';
-import { getTableById } from '../../redux/tableRedux';
+import { useSelector } from 'react-redux';
+import { Form, Row, Col, Stack, Button } from 'react-bootstrap';
+import { getTableById, updateTable } from '../../redux/tableRedux';
 
 const Table = () => {
   const { id } = useParams();
   const table = useSelector((state) => getTableById(state, id));
+
+  // console.log(table);
+
+  const [status, setStatus] = useState(table.status);
+  const [peopleAmount, setPeopleAmount] = useState(Number(table.peopleAmount));
+  const [maxPeopleAmount, setMaxPeopleAmount] = useState(Number(table.maxPeopleAmount));
+  const [bill, setBill] = useState(Number(table.bill));
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (status === 'Cleaning' || status === 'Free') {
+      setPeopleAmount('0');
+    }
+    if (status !== 'Busy') {
+      setBill('0');
+    }
+  }, [status]);
+
+  useEffect(() => {
+    if (maxPeopleAmount < peopleAmount) {
+      setPeopleAmount(maxPeopleAmount);
+    }
+  }, [peopleAmount, maxPeopleAmount]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const payload = {
+      id,
+      status,
+      bill: bill.toString(),
+      peopleAmount: peopleAmount.toString(),
+      maxPeopleAmount: maxPeopleAmount.toString(),
+    };
+    dispatch(updateTable(payload));
+    console.log(payload);
+  };
+
   return (
     <div>
       <h1 className='my-4'>Table {table.id}</h1>
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <Form.Group as={Row} className='my-3'>
           <Form.Label column sm={1}>
             <strong>Status:</strong>
           </Form.Label>
           <Col sm={4}>
-            <Form.Select>
+            <Form.Select value={status} onChange={(e) => setStatus(e.target.value)}>
               <option>Busy</option>
               <option>Reserved</option>
               <option>Free</option>
@@ -28,14 +68,26 @@ const Table = () => {
             <strong>People:</strong>
           </Form.Label>
           <Col sm={1}>
-            <Form.Control type='number' min='0' max='10'></Form.Control>
+            <Form.Control
+              type='number'
+              min='0'
+              max={maxPeopleAmount}
+              value={peopleAmount || ''}
+              onChange={(e) => setPeopleAmount(Number(e.target.value))}
+            />
           </Col>
           /
           <Col sm={1}>
-            <Form.Control type='number' min='0' max='10'></Form.Control>
+            <Form.Control
+              type='number'
+              min='0'
+              max='10'
+              value={maxPeopleAmount || ''}
+              onChange={(e) => setMaxPeopleAmount(Number(e.target.value))}
+            />
           </Col>
         </Form.Group>
-        <Form.Group as={Row} className='my-3'>
+        <Form.Group as={Row} className={status !== 'Busy' ? 'd-none' : 'my-3'}>
           <Stack direction='horizontal'>
             <Form.Label column sm={1}>
               <strong>Bill:</strong>
@@ -44,7 +96,7 @@ const Table = () => {
               <p className='m-1'>$ </p>
             </Form.Text>
             <Col sm={1}>
-              <Form.Control type='number' />
+              <Form.Control type='number' value={bill} onChange={(e) => setBill(e.target.value)} />
             </Col>
           </Stack>
         </Form.Group>
