891ec3816efb10d04a19cff7b0835fd146adb2bb
Marcin Adamiak <marcin_89@outlook.com>
2023-06-10 18:30:06 +0200

    Add structure for table page

--- src/components/pages/Table.js
+++ src/components/pages/Table.js
@@ -1,3 +1,4 @@
+import { Form, Row, Col, Stack, Button } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router';
 import { getTableById } from '../../redux/tableRedux';
@@ -8,6 +9,49 @@ const Table = () => {
   return (
     <div>
       <h1 className='my-4'>Table {table.id}</h1>
+      <Form>
+        <Form.Group as={Row} className='my-3'>
+          <Form.Label column sm={1}>
+            <strong>Status:</strong>
+          </Form.Label>
+          <Col sm={4}>
+            <Form.Select>
+              <option>Busy</option>
+              <option>Reserved</option>
+              <option>Free</option>
+              <option>Cleaning</option>
+            </Form.Select>
+          </Col>
+        </Form.Group>
+        <Form.Group as={Row} className='my-3'>
+          <Form.Label column sm={1}>
+            <strong>People:</strong>
+          </Form.Label>
+          <Col sm={1}>
+            <Form.Control type='number' min='0' max='10'></Form.Control>
+          </Col>
+          /
+          <Col sm={1}>
+            <Form.Control type='number' min='0' max='10'></Form.Control>
+          </Col>
+        </Form.Group>
+        <Form.Group as={Row} className='my-3'>
+          <Stack direction='horizontal'>
+            <Form.Label column sm={1}>
+              <strong>Bill:</strong>
+            </Form.Label>
+            <Form.Text>
+              <p className='m-1'>$ </p>
+            </Form.Text>
+            <Col sm={1}>
+              <Form.Control type='number' />
+            </Col>
+          </Stack>
+        </Form.Group>
+        <Button variant='primary' type='submit'>
+          Update
+        </Button>
+      </Form>
     </div>
   );
 };
