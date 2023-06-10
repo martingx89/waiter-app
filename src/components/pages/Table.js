d53518bb630cab4c9e2c769eabe0d3faa9d6c2c7
Marcin Adamiak <marcin_89@outlook.com>
2023-06-10 18:13:12 +0200

    Fix id

--- src/components/pages/Table.js
+++ src/components/pages/Table.js
@@ -1,9 +1,15 @@
 import { useSelector } from 'react-redux';
+import { useParams } from 'react-router';
 import { getTableById } from '../../redux/tableRedux';
 
 const Table = () => {
-  const table = useSelector((state) => getTableById);
-  return <div>Table</div>;
+  const { id } = useParams();
+  const table = useSelector((state) => getTableById(state, id));
+  return (
+    <div>
+      <h1 className='my-4'>Table {table.id}</h1>
+    </div>
+  );
 };
 
 export default Table;
