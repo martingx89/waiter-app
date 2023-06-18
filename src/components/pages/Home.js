5d28cdc780aed4c9b0e8271d65d64e5bdc351d79
Marcin Adamiak <marcin_89@outlook.com>
2023-06-18 16:37:58 +0200

    Fix home

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -15,31 +15,32 @@ const Home = () => {
 
   if (tables.length === 0) {
     return <Loading></Loading>;
+  } else {
+    return (
+      <div>
+        <h1 className='my-4'>All tables</h1>
+        <ListGroup variant='flush'>
+          {tables.map((table) => (
+            <ListGroup.Item key={table.id} {...table} className='px-0'>
+              <Stack direction='horizontal' gap={4}>
+                <h2>Table {table.id}</h2>
+                <p className='mb-1'>
+                  <strong>Status: </strong>
+                  {table.status}
+                </p>
+                <Link className='ms-auto' to={`/table/${table.id}`}>
+                  <Button variant='primary'>Show more</Button>
+                </Link>
+                <Button variant='danger' onClick={() => deleteAction(table.id)}>
+                  Delete
+                </Button>
+              </Stack>
+            </ListGroup.Item>
+          ))}
+        </ListGroup>
+      </div>
+    );
   }
-  return (
-    <div>
-      <h1 className='my-4'>All tables</h1>
-      <ListGroup variant='flush'>
-        {tables.map((table) => (
-          <ListGroup.Item key={table.id} {...table} className='px-0'>
-            <Stack direction='horizontal' gap={4}>
-              <h2>Table {table.id}</h2>
-              <p className='mb-1'>
-                <strong>Status: </strong>
-                {table.status}
-              </p>
-              <Link className='ms-auto' to={`/table/${table.id}`}>
-                <Button variant='primary'>Show more</Button>
-              </Link>
-              <Button variant='danger' onClick={() => deleteAction(table.id)}>
-                Delete
-              </Button>
-            </Stack>
-          </ListGroup.Item>
-        ))}
-      </ListGroup>
-    </div>
-  );
 };
 
 export default Home;
